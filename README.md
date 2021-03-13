a071945a1f2beb34b6ced2015b95be9ed80a1f67
Lailton <lai_kefca@hotmail.com>
2021-03-13 18:45:10 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SimpleCarsGallery
+Hello, this is a example based in https://medium.com/net-core/build-a-web-app-with-asp-net-core-and-mongodb-f9fcd61cb04f.
+
---
+++ src/CarsGalleryApp/Controllers/CarsController.cs
@@ -0,0 +1,143 @@
+﻿using CarsGalleryApp.Models;
+using CarsGalleryApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsGalleryApp.Controllers
+{
+    public class CarsController : Controller
+    {
+
+        private readonly CarService _carService;
+
+        public CarsController(CarService carService)
+        {
+            _carService = carService;
+        }
+
+        // GET: CarsController
+        public ActionResult Index()
+        {
+            return View(_carService.Get());
+        }
+
+        // GET: CarsController/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var car = _carService.Get(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+
+        // GET: CarsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                _carService.Create(car);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(car);
+        }
+
+        // GET: CarsController/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if(null == id)
+            {
+                return NotFound();
+            }
+
+            var car = _carService.Get(id);
+
+            if(null == car)
+            {
+                return NotFound();
+            }
+
+            return View(car);
+        }
+
+        // POST: CarsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, Car car)
+        {
+            if(id != car.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _carService.Update(id, car);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(car);
+            }
+        }
+
+        // GET: CarsController/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var car = _carService.Get(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+
+        // POST: CarsController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            try
+            {
+                var car = _carService.Get(id);
+
+                if (car == null)
+                {
+                    return NotFound();
+                }
+
+                _carService.Remove(car.Id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
